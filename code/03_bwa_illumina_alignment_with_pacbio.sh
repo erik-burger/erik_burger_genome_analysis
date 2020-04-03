3a9387e919fbf408c080958cb1825a3d8b86800b
Erik Bürger <erik.burger@hotmail.se>
2020-04-03 11:00:53 +0200

    code tro run BWA alignment output SAM

--- code/03_bwa_illumina_alignment_with_pacbio.sh
+++ code/03_bwa_illumina_alignment_with_pacbio.sh
@@ -11,10 +11,9 @@
 
 module load bioinfo-tools
 module load bwa/0.7.17
-module load samtools/1.10
 
 #bwa alignment that is piped to samtools to give a BAM output file instead of a SAM.
 bwa mem -t 2  /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
 /home/erbu6020/erik_burger_genome_analysis/data/raw_data/illumina/SRR6058604_scaffold_06.1P.fastq.gz \
 /home/erbu6020/erik_burger_genome_analysis/data/raw_data/illumina/SRR6058604_scaffold_06.2P.fastq.gz \
-| samtools view -Sb - > /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam
+> /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.sam
