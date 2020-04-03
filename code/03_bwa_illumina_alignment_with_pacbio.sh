ce0807f9b7c38377b805017895a0e7a79a3219b7
Erik Bürger <erik.burger@hotmail.se>
2020-04-03 10:27:54 +0200

    code tro run BWA alignment

--- code/03_bwa_illumina_alignment_with_pacbio.sh
+++ code/03_bwa_illumina_alignment_with_pacbio.sh
@@ -17,4 +17,4 @@ module load samtools/1.10
 bwa mem -t 2  /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
 /home/erbu6020/erik_burger_genome_analysis/data/raw_data/illumina/SRR6058604_scaffold_06.1P.fastq.gz \
 /home/erbu6020/erik_burger_genome_analysis/data/raw_data/illumina/SRR6058604_scaffold_06.2P.fastq.gz \
-| samtools sort -@2 -o /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam -
+| samtools sort -@2 -o /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam
