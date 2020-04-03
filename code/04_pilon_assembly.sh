d91957bb402031691bb8a933b218bde4bcc647ad
Erik Bürger <erik.burger@hotmail.se>
2020-04-03 14:14:21 +0200

    pilon analysis code

---
+++ code/04_pilon_assembly.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:45:00
+#SBATCH -J pilon_assembly
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.burger@hotmail.se
+
+
+java -Xmx16G -jar $PILON_HOME/pilon.jar \
+pilon --genome /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
+--frags /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam \
+--output pilon_assembly \
+--outdir /home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/
