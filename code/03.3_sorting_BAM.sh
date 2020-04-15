0480a6ae5472fc67a08aed8d8d6da3b94a86d4b8
Erik Bürger <erik.burger@hotmail.se>
2020-04-15 07:25:04 +0200

    added code that sorts the bam file so that it can be run in pilon

---
+++ code/03.3_sorting_BAM.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:10:00
+#SBATCH -J SAM_to_BAM
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.burger@hotmail.se
+
+module load bioinfo-tools
+module load samtools/1.10
+
+samtools sort /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam \
+> /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina_sorted.bam
