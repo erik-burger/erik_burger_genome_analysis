4ce0dc6588836c4230b4c362aeebc06e5c02d498
Erik Bürger <erik.burger@hotmail.se>
2020-04-15 07:33:58 +0200

    changed pilon bash so that it will take te correct input file

--- code/04_pilon_assembly.sh
+++ code/04_pilon_assembly.sh
@@ -12,10 +12,10 @@ module load bioinfo-tools
 module load Pilon/1.22
 module load samtools/1.10
 
-samtools index /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam
+samtools index /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina_sorted.bam
 
 java -Xmx16G -jar $PILON_HOME/pilon.jar \
 --genome /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
---frags /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina.bam \
+--frags /home/erbu6020/erik_burger_genome_analysis/analyses/03_bwa_illumina_alignment_with_pacbio/bwa_alignment_illumina_sorted.bam \
 --output pilon_assembly \
 --outdir /home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/
