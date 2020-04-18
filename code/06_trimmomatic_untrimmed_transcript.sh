fb07dcd3d51e37046161d82fe5c882b5faa5d3ac
Erik Bürger <erik.burger@hotmail.se>
2020-04-18 16:53:57 +0200

    combined input

--- code/06_trimmomatic_untrimmed_transcript.sh
+++ code/06_trimmomatic_untrimmed_transcript.sh
@@ -14,4 +14,4 @@ module load trimmomatic/0.36
 java -jar $TRIMMOMATIC_HOME/trimmomatic.jar PE -threads 2 -trimlog trimmomatic_log \
 -basein /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/untimmed/*fastq.gz \
 -baseout /home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06.fq.gz \
-ILLUMINACLIP:$TRIMMOMATIC_HOME/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+ILLUMINACLIP:/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
