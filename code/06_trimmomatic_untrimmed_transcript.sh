3c949952c4f4fb7835aa08e2984aae38086e8bad
Erik Bürger <erik.burger@hotmail.se>
2020-04-20 10:56:42 +0200

    fixed input/output

--- code/06_trimmomatic_untrimmed_transcript.sh
+++ code/06_trimmomatic_untrimmed_transcript.sh
@@ -12,6 +12,9 @@ module load bioinfo-tools
 module load trimmomatic/0.36
 
 java -jar $TRIMMOMATIC_HOME/trimmomatic.jar PE -threads 2 -trimlog trimmomatic_log \
--basein /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/untimmed/*fastq.gz \
--baseout /home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06.fq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/untimmed/SRR6040095_scaffold_06.1.fastq.gz /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/untimmed/SRR6040095_scaffold_06.2.fastq.gz\
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_forward_paired.fq.gz \
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_forward_unpaired.fq.gz  \
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_reverse_paired.fq.gz \
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_reverse_unpaired.fq.gz  \
 ILLUMINACLIP:/adapters/TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
