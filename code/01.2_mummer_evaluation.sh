35ab737025bd16757a046f777c6eae737de59f29
Erik Bürger <erik.burger@hotmail.se>
2020-04-21 15:38:35 +0200

    code to evaluate the canu assembly

---
+++ code/01.2_mummer_evaluation.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 1
+#SBATCH -t 00:15:00
+#SBATCH -J canu_mummer
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.burger@hotmail.se
+
+module load bioinfo-tools
+module load MUMmer/4.0.0beta2
+module load gnuplot/5.2.7
+
+mummer -mum -b -c /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
+/home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta > \
+/home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
+
+mummerplot --postscript --prefix=mummer_canu_assesment /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
+
+gnuplot /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.gp
