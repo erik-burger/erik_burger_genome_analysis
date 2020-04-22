674d9f4ad58dc22b1ea604962b8429d681fada27
Erik Bürger <erik.burger@hotmail.se>
2020-04-22 11:51:55 +0200

    changed to nucmer

--- code/01.2_mummer_evaluation.sh
+++ code/01.2_mummer_evaluation.sh
@@ -13,7 +13,7 @@ module load MUMmer/4.0.0beta2
 #module load gnuplot/5.2.7
 
 
-nucmer --maxmatch -p mummer_canu_assesment_unfiltered \
+nucmer --maxmatch -l 100 -c 100 -p mummer_canu_assesment_unfiltered \
 /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
 /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta 
 
