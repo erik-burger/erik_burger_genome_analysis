5891b4efa17a3be56a13f2f8d2f37e6f765438c7
Erik Bürger <erik.burger@hotmail.se>
2020-04-22 10:09:22 +0200

    added modules

--- code/02.2_multiqc_DNA.sh
+++ code/02.2_multiqc_DNA.sh
@@ -4,9 +4,12 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 00:15:00
-#SBATCH -J canu_mummer
+#SBATCH -J multi_qc_DNA
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.burger@hotmail.se
+
+module load bioinfo-tools
+module load MultiQC/1.8
 
 multiqc -n DNA_multiqc.html \
 -o /home/erbu6020/erik_burger_genome_analysis/analyses/02_fastQC_illumina_DNA \
