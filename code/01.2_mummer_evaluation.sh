daf3d066ee75c35f68b7a4fd310516ffae88d428
Erik Bürger <erik.burger@hotmail.se>
2020-04-22 10:40:38 +0200

    changed to nucmer

--- code/01.2_mummer_evaluation.sh
+++ code/01.2_mummer_evaluation.sh
@@ -9,14 +9,18 @@
 #SBATCH --mail-user erik.burger@hotmail.se
 
 module load bioinfo-tools
-module load MUMmer/3.23
+module load MUMmer/4.0.0beta2
 module load gnuplot/5.2.7
 
 
-mummer -mum -b -c /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
-/home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
-> /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
+nucmer -maxmatch -p mummer_canu_assesment_unfiltered \
+/home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
+/home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta 
 
-mummerplot -postscript -prefix=mummer_canu_assesment /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
+delta-filter mummer_canu_assesment_unfiltered.delta mummer_canu_assesment.delta
 
-gnuplot /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.gp
+mummerplot --png -p mummer_canu_assesment.delta
+
+#mummerplot -postscript -prefix=mummer_canu_assesment /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
+
+#gnuplot /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.gp
