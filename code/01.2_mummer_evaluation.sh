ce3657e6bb3c1d3f54d4981c29ac24da4c609557
Erik Bürger <erik.burger@hotmail.se>
2020-04-22 09:18:35 +0200

    added parameters

--- code/01.2_mummer_evaluation.sh
+++ code/01.2_mummer_evaluation.sh
@@ -9,11 +9,11 @@
 #SBATCH --mail-user erik.burger@hotmail.se
 
 module load bioinfo-tools
-module load MUMmer/3.9.4alpha
+module load MUMmer/3.22
 module load gnuplot/5.2.7
 
 
-mummer -mum /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
+mummer -mum –b –c /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
 /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta \
 > /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/mummer_canu_assesment.mums
 
