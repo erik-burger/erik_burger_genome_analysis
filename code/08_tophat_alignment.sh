7672bf56325df26c683f07a085aac11e7593977e
Erik Bürger <erik.burger@hotmail.se>
2020-04-24 09:51:12 +0200

    index teh sequence using bowtie

--- code/08_tophat_alignment.sh
+++ code/08_tophat_alignment.sh
@@ -3,15 +3,18 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 03:00:00
+#SBATCH -t 06:00:00
 #SBATCH -J tophat_alignment
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.burger@hotmail.se
 
 module load bioinfo-tools
 module load tophat/2.1.1
+module load bowtie2/2.3.5.1
+
+bowtie2-build --threads 2 /home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/pilon_assembly.fasta pilon_assembly_index
 
 tophat -p 2 -o /home/erbu6020/erik_burger_genome_analysis/analyses/08_tophat_alignment \
-/home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/pilon_assembly.fasta \
+home/erbu6020/erik_burger_genome_analysis/analyses/08_tophat_alignment/*.bt2 \
 /home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/*paired.fq.gz\
 /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/*.fastq.gz
