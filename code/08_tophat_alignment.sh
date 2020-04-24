38fc46a276b5a329372602a908e7b0f96eaae5b9
Erik Bürger <erik.burger@hotmail.se>
2020-04-24 08:54:27 +0200

    code to run tophat

--- code/08_tophat_alignment.sh
+++ code/08_tophat_alignment.sh
@@ -12,3 +12,6 @@ module load bioinfo-tools
 module load tophat/2.1.1
 
 tophat -p 2 -o /home/erbu6020/erik_burger_genome_analysis/analyses/08_tophat_alignment \
+/home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/pilon_assembly.fasta \
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/*paired.fq.gz\
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/*.fastq.gz
