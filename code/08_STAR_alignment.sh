57e4029905ec16548cbceb7064a510684e93398f
Erik Bürger <erik.burger@hotmail.se>
2020-04-24 15:30:54 +0200

    increased the time allocation from 6h to 10h

--- code/08_STAR_alignment.sh
+++ code/08_STAR_alignment.sh
@@ -3,7 +3,7 @@
 #SBATCH -A g2020008
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 06:00:00
+#SBATCH -t 10:00:00
 #SBATCH -J tophat_alignment
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.burger@hotmail.se
