fd07cd3194d5517c0c2460db197f75ad1899e6cf
Erik Bürger <erik.burger@hotmail.se>
2020-04-24 08:41:05 +0200

    fixed some syntax

--- code/01.3_quast_evaluation.sh
+++ code/01.3_quast_evaluation.sh
@@ -9,8 +9,8 @@
 #SBATCH --mail-user erik.burger@hotmail.se
 
 module load bioinfo-tools
-module load quast/4.5.4
+module load quast
 
-python3 quast.py -o /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly \
--r /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
+quast.py -R /home/erbu6020/erik_burger_genome_analysis/data/paper_data/Contig_06_paper_assembly.fasta \
+-o /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly \
 /home/erbu6020/erik_burger_genome_analysis/analyses/01_canu_pacbio_assembly/canu_durian_alignment.contigs.fasta
