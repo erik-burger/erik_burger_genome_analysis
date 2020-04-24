7b99828f281dd9a83d236b65bcf9d7e1bffdb3b9
Erik Bürger <erik.burger@hotmail.se>
2020-04-24 15:26:32 +0200

    decided to use STAR instead of tophat

---
+++ code/08_STAR_alignment.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -l
+
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 06:00:00
+#SBATCH -J tophat_alignment
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user erik.burger@hotmail.se
+
+module load bioinfo-tools
+module spider star/2.7.2b
+
+# Create index files
+star --runTHreadN 2 --runMode genomeGenerate \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--genomeFastaFiles /home/erbu6020/erik_burger_genome_analysis/analyses/04_pilon_assembly/pilon_assembly.fasta \
+--sjdbOverhang 99
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040092/
+# SRR6040092
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040092_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040092_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040093/
+# SRR6040093
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040093_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040093_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040094/
+# SRR6040094
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040094_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040094_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040095/
+# SRR6040095
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_forward_paired.fq.gz \
+/home/erbu6020/erik_burger_genome_analysis/analyses/06_trimmomatic_untrimmed_transcript/SRR6040095_scaffold_06_reverse_paired.fq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040096/
+# SRR6040096
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040096_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040096_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040097/
+# SRR6040097
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040097_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6040097_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6156066/
+# SRR6156066
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156066_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156066_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6156067/
+# SRR6156067
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156067_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156067_scaffold_06.2.fastq.gz
+
+cd /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6156069/
+# SRR6156069
+star --runTHreadN 2 \
+--genomeDir /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/indexed_genome \
+--readFilesIn /home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156069_scaffold_06.1.fastq.gz \
+/home/erbu6020/erik_burger_genome_analysis/data/raw_data/transcriptome/trimmed/SRR6156069_scaffold_06.2.fastq.gz
