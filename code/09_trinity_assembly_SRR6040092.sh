7689bd51f4b4880a350e7b535bcd7a0e0e011c0a
Erik Bürger <erik.burger@hotmail.se>
2020-04-28 07:01:13 +0200

    spelling

--- code/09_trinity_assembly_SRR6040092.sh
+++ code/09_trinity_assembly_SRR6040092.sh
@@ -13,10 +13,10 @@ module load trinity/2.9.1
 module load samtools/1.10
 
 samtools view -Sb /home/erbu6020/erik_burger_genome_analysis/analyses/08_STAR_alignment/SRR6040092/Aligned.out.sam \
-/home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam
+> /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam
 
 samtools sort /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam \
-/home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_sorted_SRR6040092.bam
+> /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_sorted_SRR6040092.bam
 
 Trinity --genome_guided_bam /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_sorted_SRR6040092.bam \
 --genome_guided_max_intron 10000 --max_memory 10G --CPU 4 
