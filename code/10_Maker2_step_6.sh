4255b845a6adc84631fed137a1e8bdcc2459de88
Erik Bürger <erik.burger@hotmail.se>
2020-05-05 16:11:28 +0200

    changed the config path

--- code/10_Maker2_step_6.sh
+++ code/10_Maker2_step_6.sh
@@ -11,4 +11,5 @@
 module load bioinfo-tools
 module load augustus/3.3.3
 
-optimize_augustus.pl --species=durian augustus.gbk.train --cpus=4
+optimize_augustus.pl --species=durian augustus.gbk.train --cpus=4 \ 
+--AUGUSTUS_CONFIG_PATH= /home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/augustus/augustus_config
