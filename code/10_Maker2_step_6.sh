dad60073de0e494f5f2b098327662ffddae53b87
Erik Bürger <erik.burger@hotmail.se>
2020-05-06 11:07:19 +0200

    code to run the optimization

--- code/10_Maker2_step_6.sh
+++ code/10_Maker2_step_6.sh
@@ -2,8 +2,8 @@
 
 #SBATCH -A g2020008
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 6:00:00
+#SBATCH -n 4
+#SBATCH -t 24:00:00
 #SBATCH -J Augustus_step_6
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.burger@hotmail.se
@@ -11,5 +11,5 @@
 module load bioinfo-tools
 module load augustus/3.3.3
 
-optimize_augustus.pl --species=durian augustus.gbk.train --cpus=2 \ 
+optimize_augustus.pl --species=durian augustus.gbk.train --cpus=4 \ 
 --AUGUSTUS_CONFIG_PATH= /home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/augustus/augustus_config
