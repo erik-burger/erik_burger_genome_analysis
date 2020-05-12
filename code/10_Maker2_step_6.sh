71a7eff286ec7e6d130cbd66f3f316ca9d9ad9f0
Erik Bürger <erik.burger@hotmail.se>
2020-05-12 17:25:23 +0200

    Typo fix

--- code/10_Maker2_step_6.sh
+++ code/10_Maker2_step_6.sh
@@ -12,4 +12,4 @@ module load bioinfo-tools
 module load augustus/3.3.3
 
 optimize_augustus.pl --species=durian augustus.gbk.train --cpus=4 \ 
---AUGUSTUS_CONFIG_PATH= /home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/augustus/augustus_config
+--AUGUSTUS_CONFIG_PATH=/home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/augustus/augustus_config
