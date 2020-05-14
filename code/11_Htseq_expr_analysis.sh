09c53e7a1679db8ae0fe68f6c174459ab84edc0f
Erik Bürger <erik.burger@hotmail.se>
2020-05-14 11:11:46 +0200

    code to run htseq

--- code/11_Htseq_expr_analysis.sh
+++ code/11_Htseq_expr_analysis.sh
@@ -4,10 +4,15 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 12:00:00
-#SBATCH -J Augustus_step_6
+#SBATCH -J Htseq_11
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user erik.burger@hotmail.se
 
 module load bioinfo-tools
-module load kgjbaoutngpne
+module load htseq/0.9.1
+
+
+# SRR6040092
+htseq-count -f bam /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam \ 
+/home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/pilon_assembly.all.gff
 
