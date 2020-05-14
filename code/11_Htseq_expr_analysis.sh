06494566bad30eba1cd3236c2fa0ccdfaab88bdf
Erik Bürger <erik.burger@hotmail.se>
2020-05-14 11:23:08 +0200

    changed the gene atribute variable

--- code/11_Htseq_expr_analysis.sh
+++ code/11_Htseq_expr_analysis.sh
@@ -13,5 +13,5 @@ module load htseq/0.9.1
 
 
 # SRR6040092
-htseq-count -f bam /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam /home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/pilon_assembly.all.gff
+htseq-count --idattr=gene -f bam /home/erbu6020/erik_burger_genome_analysis/analyses/09_trinity_assembly_SRR6040092/Aligned_SRR6040092.bam /home/erbu6020/erik_burger_genome_analysis/analyses/10_Maker2_annotation/pilon_assembly.maker.output/pilon_assembly.all.gff
 
